6a1eda9f2145571535361134a44a8fa2f9589742
Tim-Gaede <TimGaede@gmail.com>
2018-11-05 13:25:14 -0500

    Update function sortComparable!(array, compareTo).jl

--- function sortComparable!(array, compareTo).jl
+++ function sortComparable!(array, compareTo).jl
@@ -45,49 +45,4 @@ end
 
 
 
-
-
-
-
-function sortComparableAlternative!(array, compareTo)
-    if (length(array) < 16)
-
-        #insertionSort_comparable!(array, compareTo) ...........................
-        len = length(array)
-
-        for i = 2 : len
-            j = i
-            while (j > 1    &&    compareTo(array[j], array[j-1]) < 0)
-                temp = array[j];    array[j] = array[j-1];    array[j-1] = temp
-                j -= 1
-            end
-        end
-        # ......................................................................
-
-    else
-
-        quickSort_comparable!(array, 1, length(array), compareTo)
-
-        #-----------------------------------------------------------------------
-        function quickSort_comparable!(array, lo, hi, compareTo)
-            i, j = lo, hi
-            while i < hi
-                pivot = array[(lo+hi)>>>1]
-                while i <= j
-                    while compareTo(array[i], pivot) < 0;  i += 1 end
-                    while compareTo(array[j], pivot) > 0;  j -= 1 end
-                    if i <= j
-                        array[i], array[j] = array[j], array[i]
-                        i += 1;  j -= 1
-                    end
-                end
-                if lo < j; quickSort_comparable!(array, lo, j, compareTo) end
-                lo, j  =  i, hi
-            end
-        end
-        # ----------------------------------------------------------------------
-
-    end
-end
-
  
