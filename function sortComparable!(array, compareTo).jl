26cc2973f02bba17894bd1dff5d22ce905a81c64
Timothy Gaede <TimGaede@gmail.com>
2019-10-31 06:10:02 -0400

    Update function sortComparable!(array, compareTo).jl

--- function sortComparable!(array, compareTo).jl
+++ function sortComparable!(array, compareTo).jl
@@ -1,46 +1,43 @@
-function sortComparable!(array, compareTo)
+function sortComparable!(a, compareTo)
 
-    function insertionSort_comparable!(array, compareTo)
+    #inertionSort for comparable
+    function insSort_comp!(a, compareTo)
 
-        for i = 2 : length(array)
+        for i = 2 : length(a)
             j = i
-            while j > 1    &&    compareTo(array[j], array[j-1]) < 0
-                array[j], array[j-1]  =  array[j-1], array[j] # swap
+            while j > 1  &&  compareTo(a[j], a[j-1]) < 0
+                a[j], a[j-1]  =  a[j-1], a[j] # swap
                 j -= 1
             end
         end
 
     end
 
-
-    function quickSort_comparable!(array, lo, hi, compareTo)
+    #quickSort for comparable
+    function qSort_comp!(a, lo, hi, compareTo)
 
         i = lo;  j = hi
         while i < hi
-            pivot = array[(lo+hi)>>>1]
+            pivot = a[(lo+hi)>>>1]
             while i <= j
-                while compareTo(array[i], pivot) < 0;  i += 1 end
-                while compareTo(array[j], pivot) > 0;  j -= 1 end
+                while compareTo(a[i], pivot) < 0;    i += 1;    end
+                while compareTo(a[j], pivot) > 0;    j -= 1;    end
                 if i <= j
-                    array[i], array[j]  =  array[j], array[i] # swap
+                    a[i], a[j]  =  a[j], a[i] # swap
                     i += 1;  j -= 1
                 end
             end
-            if lo < j;  quickSort_comparable!(array, lo, j, compareTo) end
+            if lo < j;     qSort_comp!(a, lo, j, compareTo);    end
             lo = i;  j = hi
         end
 
     end
 
 
-    if (length(array) < 16) # <-Not optimized?
-        insertionSort_comparable!(array, compareTo)
+    if length(a) < 16 # <-Not optimized?
+        insSort_comp!(a, compareTo)
     else
-        quickSort_comparable!(array, 1, length(array), compareTo)
+        qSort_comp!(a, 1, length(a), compareTo)
     end
 
 end
-
-
-
- 
